70323c542894397381f85f469e5c2c60ba98ea93
Guest User <Guest@admins-iMac-5.local>
2015-03-26 14:43:57 +0900

    update and name file

--- castlewars/Classes/Game.cpp
+++ castlewars/Classes/Game.cpp
@@ -1,27 +1,27 @@
-#include "HelloWorldScene.h"
+#include "Game.h"
 #include "SimpleAudioEngine.h"
 #include <string>
 
 using namespace cocos2d;
 using namespace CocosDenshion;
 
-CCScene* HelloWorld::scene()
+CCScene* Game::scene()
 {
     // 'scene' is an autorelease object
     CCScene *scene = CCScene::create();
     
     // 'layer' is an autorelease object
-    HelloWorld *layer = HelloWorld::create();
-
+    Game *layer = Game::create();
+    
     // add layer as a child to scene
     scene->addChild(layer);
-
+    
     // return the scene
     return scene;
 }
 
 // on "init" you need to initialize your instance
-bool HelloWorld::init()
+bool Game::init()
 {
     //////////////////////////////
     // 1. super init first
@@ -29,64 +29,71 @@ bool HelloWorld::init()
     {
         return false;
     }
-
+    
     /////////////////////////////
     // 2. add a menu item with "X" image, which is clicked to quit the program
     //    you may modify it.
-
+    
     // add a "close" icon to exit the progress. it's an autorelease object
     CCMenuItemImage *pCloseItem = CCMenuItemImage::create(
-                                        "CloseNormal.png",
-                                        "CloseSelected.png",
-                                        this,
-                                        menu_selector(HelloWorld::menuCloseCallback) );
+                                                          "CloseNormal.png",
+                                                          "CloseSelected.png",
+                                                          this,
+                                                          menu_selector(Game::menuCloseCallback) );
     pCloseItem->setPosition( ccp(CCDirector::sharedDirector()->getWinSize().width - 20, 20) );
     
-    CCMenuItemImage *turnButton = CCMenuItemImage::create("CloseNormal.png", "CloseSelected.png", this, menu_selector(HelloWorld::nextTurn));
+    CCMenuItemImage *turnButton = CCMenuItemImage::create("CloseNormal.png", "CloseSelected.png", this, menu_selector(Game::nextTurn));
     
     // create menu, it's an autorelease object
     CCMenu* pMenu = CCMenu::create(pCloseItem, turnButton, NULL);
     pMenu->setPosition( CCPointZero );
     this->addChild(pMenu, 1);
-
+    
     /////////////////////////////
     // 3. add your codes below...
-
+    
     // add a label shows "Hello World"
     // create and initialize a label
     CCString turnStr = *CCString::createWithFormat("%d", this->turn);
     CCLabelTTF* pLabel = CCLabelTTF::create(turnStr.getCString(), "Thonburi", 42);
-
+    
     // ask director the window size
     CCSize size = CCDirector::sharedDirector()->getWinSize();
-
+    
     // position the label on the center of the screen
     pLabel->setPosition( ccp(size.width / 2, size.height - 20) );
-
+    
     // add the label as a child to this layer
     this->addChild(pLabel, 1);
-
-    // add "HelloWorld" splash screen"
-
+    
+    // add "Game" splash screen"
+    
     // position the sprite on the center of the screen
     turnButton->setPosition(ccp(size.width / 2, size.height / 2));
-
+    
     // add the sprite as a child to this layer
-   // this->addChild(turnButton, 0);
+    // this->addChild(turnButton, 0);
+    
+    this->schedule(schedule_selector(Game::update));
     
     return true;
 }
 
-void HelloWorld::nextTurn(CCObject *pSend)
+void Game::update(float dt)
+{
+    
+}
+
+void Game::nextTurn(CCObject *pSend)
 {
     this->turn += 1;
     printf("%d", this->turn);
 }
-                                                          
-void HelloWorld::menuCloseCallback(CCObject* pSender)
+
+void Game::menuCloseCallback(CCObject* pSender)
 {
     CCDirector::sharedDirector()->end();
-
+    
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
     exit(0);
 #endif
--- castlewars/Classes/Game.h
+++ castlewars/Classes/Game.h
@@ -1,9 +1,9 @@
-#ifndef __HELLOWORLD_SCENE_H__
-#define __HELLOWORLD_SCENE_H__
+#ifndef __GAME_SCENE_H__
+#define __GAME_SCENE_H__
 
 #include "cocos2d.h"
 
-class HelloWorld : public cocos2d::CCLayer
+class Game : public cocos2d::CCLayer
 {
 private:
     int turn;
@@ -11,6 +11,7 @@ private:
 public:
     // Method 'init' in cocos2d-x returns bool, instead of 'id' in cocos2d-iphone (an object pointer)
     virtual bool init();
+    virtual void update(float);
     
     // there's no 'id' in cpp, so we recommend to return the class instance pointer
     static cocos2d::CCScene* scene();
@@ -19,9 +20,9 @@ public:
     void menuCloseCallback(CCObject* pSender);
     
     void nextTurn(CCObject *pSend);
-
+    
     // preprocessor macro for "static create()" constructor ( node() deprecated )
-    CREATE_FUNC(HelloWorld);
+    CREATE_FUNC(Game);
 };
 
 #endif // __HELLOWORLD_SCENE_H__
