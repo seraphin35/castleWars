260cd90819270da5459a0f69b073c4c4f5fb0dab
Guest User <Guest@admins-iMac-103.local>
2015-04-07 15:09:43 +0900

    merged cards and card files

--- castlewars/Card.cpp
+++ castlewars/Card.cpp
@@ -28,3 +28,242 @@ const cocos2d::CCSprite* Card::getImage()
 {
     return this->image;
 }
+
+void Card::damageCastle(Player *player, int damage)
+{
+    int castle = player->getCastle();
+    
+    castle -= damage;
+    player->setCastle(castle);
+}
+
+
+void Card::damage(Player *player, int damage) {
+    int wall = player->getWall();
+    
+    wall -= damage;
+    if (wall <= 0) {
+        damageCastle(player, -wall);
+    }
+    player->setWall(wall);
+}
+
+bool Card::stripMining(Player *player1, Player *player2)
+{
+    int magic = player1->getMagic();
+    
+    magic -= 1;
+    if (magic < 0)
+        magic = 0;
+    player1->setMagic(magic);
+    player1->setWall(player1->getWall() + 10);
+    player1->setCrystals(player1->getCrystals() + 5);
+    
+    return false;
+}
+
+bool Card::stoneGiant(Player *player1, Player *player2)
+{
+    damage(player2, 10);
+    player1->setWall(player1->getWall() + 4);
+    
+    return false;
+}
+
+bool Card::sheepishRabbit(Player *player1, Player *player2)
+{
+    damage(player2, 6);
+    player2->setCrystals(player1->getCrystals() - 3);
+    
+    return false;
+}
+
+bool Card::rubyWand(Player *player1, Player *player2)
+{
+    player1->setCastle(player1->getCastle() + 5);
+    
+    return false;
+}
+
+bool Card::rockSlasher(Player *player1, Player *player2)
+{
+    damage(player2, 6);
+    
+    return false;
+}
+
+bool Card::recycledRainbows(Player *player1, Player *player2)
+{
+    player1->setCastle(player1->getCastle() + 1);
+    player2->setCastle(player2->getCastle() + 1);
+    player1->setCrystals(player1->getCrystals() + 3);
+    
+    return false;
+}
+
+bool Card::protectionWard(Player *player1, Player *player2)
+{
+    player1->setCastle(player1->getCastle() + 8);
+    player1->setWall(player2->getWall() + 3);
+    
+    return false;
+}
+
+bool Card::mortarMole(Player *player1, Player *player2)
+{
+    damageCastle(player2, 4);
+    
+    return false;
+}
+
+bool Card::mobbinGoblin(Player *player1, Player *player2)
+{
+    damage(player1, 3);
+    damage(player2, 6);
+    
+    return false;
+}
+
+bool Card::manaStompers(Player *player1, Player *player2)
+{
+    int magic;
+    
+    damage(player2, 8);
+    magic -= 1;
+    if (magic < 0)
+        magic = 0;
+    player2->setMagic(magic);
+    
+    return false;
+}
+
+bool Card::manaDisease(Player *player1, Player *player2)
+{
+    int p1Gems;
+    int p2Gems;
+    
+    p1Gems = player1->getCrystals();
+    p2Gems = player2->getCrystals();
+    p1Gems -= 8;
+    p2Gems -= 8;
+    
+    if (p1Gems < 0)
+        p1Gems = 0;
+    if (p2Gems < 0)
+        p2Gems = 0;
+    player1->setCrystals(p1Gems);
+    player2->setCrystals(p2Gems);
+    
+    return false;
+}
+
+bool Card::magicMiners(Player *player1, Player *player2)
+{
+    player1->setWall(player1->getWall() + 4);
+    player1->setMagic(player1->getMagic() + 4);
+    
+    return false;
+}
+
+bool Card::instantWall(Player *player1, Player *player2)
+{
+    player1->setWall(player1->getWall() + 3);
+    
+    return false;
+}
+
+bool Card::insecureWall(Player *player1, Player *player2)
+{
+    player1->setWall(player1->getWall() + 4);
+    
+    return false;
+}
+
+bool Card::harmonicOrc(Player *player1, Player *player2)
+{
+    player1->setWall(player1->getWall() + 6);
+    player1->setCastle(player1->getCastle() + 7);
+    
+    return false;
+}
+
+bool Card::friendship(Player *player1, Player *player2)
+{
+    player1->setWall(player1->getWall() + 1);
+    
+    return true;
+}
+
+bool Card::flyinGoblin(Player *player1, Player *player2)
+{
+    damage(player2, 2);
+    
+    return true;
+}
+
+bool Card::emeraldWand(Player *player1, Player *player2)
+{
+    player1->setCastle(player1->getCastle() + 8);
+    
+    return false;
+}
+
+bool Card::clubbinGoblin(Player *player1, Player *player2)
+{
+    damageCastle(player1, 3);
+    damage(player2, 8);
+    
+    return false;
+}
+
+bool Card::bowminGoblin(Player *player1, Player *player2)
+{
+    damageCastle(player2, 3);
+    damage(player1, 1);
+    
+    return false;
+}
+
+bool Card::bottledFlatulence(Player *player1, Player *player2)
+{
+    damageCastle(player2, 3);
+    
+    return false;
+}
+
+bool Card::amethystWand(Player *player1, Player *player2)
+{
+    player1->setCastle(player1->getCastle() + 3);
+    
+    return false;
+}
+
+std::vector<Card *> &Card::getNewDeck()
+{
+    std::vector<Card *> *deck = new std::vector<Card *>();
+    
+    deck->push_back(new Card(&amethystWand, 4, cocos2d::CCSprite::create("amethyst_wand.png")));
+    deck->push_back(new Card(&bottledFlatulence, 4, cocos2d::CCSprite::create("bottled_flatulence.png")));
+    deck->push_back(new Card(&bowminGoblin, 4, cocos2d::CCSprite::create("bowmin_goblin.png")));
+    deck->push_back(new Card(&clubbinGoblin, 4, cocos2d::CCSprite::create("clubin_goblin.png")));
+    deck->push_back(new Card(&emeraldWand, 10, cocos2d::CCSprite::create("emerald_wand.png")));
+    deck->push_back(new Card(&flyinGoblin, 3, cocos2d::CCSprite::create("flyin_goblin.png")));
+    deck->push_back(new Card(&friendship, 2, cocos2d::CCSprite::create("friendship.png")));
+    deck->push_back(new Card(&harmonicOrc, 7, cocos2d::CCSprite::create("harmonic_orc.png")));
+    deck->push_back(new Card(&insecureWall, 3, cocos2d::CCSprite::create("insecure_wall.png")));
+    deck->push_back(new Card(&instantWall, 2, cocos2d::CCSprite::create("instant_wall.png")));
+    deck->push_back(new Card(&magicMiners, 4, cocos2d::CCSprite::create("magic_miners.png")));
+    deck->push_back(new Card(&manaDisease, 0, cocos2d::CCSprite::create("mana_disease.png")));
+    deck->push_back(new Card(&manaStompers, 11, cocos2d::CCSprite::create("mana_stompers.png")));
+    deck->push_back(new Card(&mobbinGoblin, 3, cocos2d::CCSprite::create("mobbin_goblin.png")));
+    deck->push_back(new Card(&mortarMole, 6, cocos2d::CCSprite::create("mortal_mole.png")));
+    deck->push_back(new Card(&protectionWard, 12, cocos2d::CCSprite::create("protection_ward.png")));
+    deck->push_back(new Card(&recycledRainbows, 0, cocos2d::CCSprite::create("recycled_rainbow.png")));
+    deck->push_back(new Card(&rockSlasher, 5, cocos2d::CCSprite::create("rock_slasher.png")));
+    deck->push_back(new Card(&rubyWand, 6, cocos2d::CCSprite::create("ruby_wand.png")));
+    deck->push_back(new Card(&sheepishRabbit, 9, cocos2d::CCSprite::create("sheepish_rabbit.png")));
+    deck->push_back(new Card(&stoneGiant, 12, cocos2d::CCSprite::create("stone_giant.png")));
+    deck->push_back(new Card(&stripMining, 0, cocos2d::CCSprite::create("strip_mining.png")));
+    
+    return *deck;
+}
--- castlewars/Card.h
+++ castlewars/Card.h
@@ -18,11 +18,12 @@ class Player;
 typedef bool (*ptrfunc)(Player *, Player *);
 
 class Card {
-
+    
 private:
     int value;
     ptrfunc effect;
     cocos2d::CCSprite* image;
+    
 public:
     Card(ptrfunc effect, int value, cocos2d::CCSprite* image);
     ~Card() {}
@@ -31,6 +32,34 @@ public:
     const ptrfunc getEffect();
     const cocos2d::CCSprite* getImage();
     //image getter here
+    
+    static void damage(Player *player, int damage);
+    static void damageCastle(Player *player, int damage);
+    
+    static bool stripMining(Player *player1, Player *player2);
+    static bool stoneGiant(Player *player1, Player *player2);
+    static bool sheepishRabbit(Player *player1, Player *player2);
+    static bool rubyWand(Player *player1, Player *player2);
+    static bool rockSlasher(Player *player1, Player *player2);
+    static bool recycledRainbows(Player *player1, Player *player2);
+    static bool protectionWard(Player *player1, Player *player2);
+    static bool mortarMole(Player *player1, Player *player2);
+    static bool mobbinGoblin(Player *player1, Player *player2);
+    static bool manaStompers(Player *player1, Player *player2);
+    static bool manaDisease(Player *player1, Player *player2);
+    static bool magicMiners(Player *player1, Player *player2);
+    static bool instantWall(Player *player1, Player *player2);
+    static bool insecureWall(Player *player1, Player *player2);
+    static bool harmonicOrc(Player *player1, Player *player2);
+    static bool friendship(Player *player1, Player *player2);
+    static bool flyinGoblin(Player *player1, Player *player2);
+    static bool emeraldWand(Player *player1, Player *player2);
+    static bool clubbinGoblin(Player *player1, Player *player2);
+    static bool bowminGoblin(Player *player1, Player *player2);
+    static bool bottledFlatulence(Player *player1, Player *player2);
+    static bool amethystWand(Player *player1, Player *player2);
+    
+    static std::vector<Card *> &getNewDeck();
 };
 
 #endif /* defined(__castlewars__File__) */
